20bb89e9df57080e91b4337344d72a1d0a504211
parmarbalvant23 <balvantparmar770@gmail.com>
2024-07-08 11:58:01 +0530

    Add files via upload

---
+++ task 1.cpp
@@ -0,0 +1,23 @@
+#include<stdio.h>
+
+main()
+
+{
+	
+	int  a,b;
+	
+	
+	printf("addition of %d and %d =%d\n",a,b,a+b);
+	printf("subtraction of %d and %d = %d\n",a,b,a-b);
+	printf("multiplication of %d and %d = %\n",a,b,a*b);
+	printf("division of %d and %d = %d\n",a,b,a/b);
+	printf("modul of %d and %d = %d\n",a,b,a%b);
+	
+	
+	printf("%d + %d = %d\n",a,b,a+b);
+	printf("%d - %d = %d\n",a,b,a-b);
+	printf("%d * %d = %d\n",a,b,a*b);
+	printf("%d / %d = %d\n",a,b,a/b);
+	printf("%d % %d = %d\n",a,b,a%b);
+}
+
