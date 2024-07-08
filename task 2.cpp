3972f6edce483c92a8475725e9f352f21adf382d
parmarbalvant23 <balvantparmar770@gmail.com>
2024-07-08 12:03:54 +0530

    Add files via upload

---
+++ task 2.cpp
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+
+main()
+
+{
+	
+	   float r,area;
+	   
+	   printf("enter redius of circle =");
+	   scanf("%f" ,&r);
+	   
+	   area = 3.14 *r*r;
+	   printf("area of circle = %.2f",area);
+}
